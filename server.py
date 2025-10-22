6ec31b777a4f5d53809dfad88fd62f33230e1a98
Jonas <jonasg@umich.edu>
2025-10-21 20:48:08 -0400

    Basic search and reindex endpoints

--- server.py
+++ server.py
@@ -1,16 +1,31 @@
 from fastapi import FastAPI
+from opensearchpy import OpenSearch
+
+from apple_notes import index_apple_notes, query_apple_notes, read_apple_notes
+from imessage import index_imessages, query_imessages, read_new_imessages
 
 app = FastAPI(title="Ark")
 
 
 @app.get("/search")
 def search(query: str):
-    return {"results": []}
+    os_client = OpenSearch(hosts=[{"host": "localhost", "port": 9200}], use_ssl=False)
+    imessage_results = query_imessages(os_client, query)
+    apple_note_results = query_apple_notes(os_client, query)
+    return {"results": imessage_results + apple_note_results}
 
 
 @app.post("/reindex")
 def reindex():
-    return {"errors": []}
+    os_client = OpenSearch(hosts=[{"host": "localhost", "port": 9200}], use_ssl=False)
+    apple_notes = read_apple_notes()
+    apple_note_indexed_count, apple_note_indexing_errors = index_apple_notes(os_client, apple_notes)
+    imessages = read_new_imessages()
+    imessage_indexed_count, imessage_indexing_errors = index_imessages(os_client, imessages)
+    return {
+        "count": apple_note_indexed_count + imessage_indexed_count,
+        "errors": apple_note_indexing_errors + imessage_indexing_errors,
+    }
 
 
 if __name__ == "__main__":
